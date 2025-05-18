e6933fbcd1743d89bc5be730063939fd3094a436
FispBG <aatulupov@stud.etu.ru>
2025-05-19 01:47:40 +0300

    111

--- doctor_app/admin.py
+++ doctor_app/admin.py
@@ -2,6 +2,26 @@ from django import forms
 from django.contrib import admin
 from .models import Doctor, Symptom, Appointment, Contact, CustomUser, DoctorSymptomGroup
 
+SPECIALIZATION_SYMPTOMS_MAP = {
+    'Терапевт': [
+        'Кашель', 'Насморк', 'Температура', 'Боль в горле',
+        'Слабость', 'Головная боль', 'Боль в мышцах'
+    ],
+    'Кардиолог': [
+        'Боль в груди', 'Одышка', 'Учащенное сердцебиение',
+        'Отек ног', 'Головокружение', 'Повышенное давление'
+    ],
+    'Невролог': [
+        'Головная боль', 'Головокружение', 'Нарушение сна',
+        'Онемение конечностей', 'Слабость в конечностях', 'Тремор',
+        'Нарушение координации', 'Боли в спине'
+    ],
+    'Дерматолог': [
+        'Сыпь', 'Зуд', 'Покраснение кожи', 'Сухость кожи',
+        'Высыпания', 'Изменение пигментации', 'Шелушение'
+    ],
+}
+
 @admin.register(DoctorSymptomGroup)
 class DoctorSymptomGroupAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -13,28 +33,52 @@ class DoctorForm(forms.ModelForm):
         fields = '__all__'
 
     def clean_specialization(self):
-        specialization = self.cleaned_data.get('specialization') # Получаем специализацию доктора (например, 'Терапевт')
-        if specialization:
-            # ИСПРАВЛЕННАЯ СТРОКА:
-            # Ищем DoctorSymptomGroup, у которой поле 'name' совпадает со специализацией доктора.
-            category = DoctorSymptomGroup.objects.filter(name=specialization).first()
-            
-            if category:
-                # Если найдена соответствующая группа симптомов,
-                # устанавливаем связанные симптомы для данного экземпляра доктора.
-                # ВАЖНО: Это действие перезапишет любые симптомы,
-                # которые пользователь мог выбрать вручную в поле 'related_symptoms' в админке,
-                # если категория найдена.
-                self.instance.related_symptoms.set(category.symptoms.all())
+        specialization = self.cleaned_data.get('specialization')
         return specialization
 
 class DoctorAdmin(admin.ModelAdmin):
     form = DoctorForm
     list_display = ('name', 'specialization')
-    filter_horizontal = ('related_symptoms',) # Позволяет редактировать related_symptoms в админке
+    filter_horizontal = ('related_symptoms',) 
+
+    def save_model(self, request, obj, form, change):
+
+        original_specialization = None
+        if change and obj.pk:
+            try:
+                original_specialization = Doctor.objects.get(pk=obj.pk).specialization
+            except Doctor.DoesNotExist:
+                pass
+
+        super().save_model(request, obj, form, change)
+
+        new_specialization = obj.specialization
+
+        if change and original_specialization != new_specialization and new_specialization:
+            
+
+            symptom_names_for_new_spec = SPECIALIZATION_SYMPTOMS_MAP.get(new_specialization, [])
+            symptoms_to_set = []
+            if symptom_names_for_new_spec:
+                for name in symptom_names_for_new_spec:
+                    symptom, _ = Symptom.objects.get_or_create(name=name)
+                    symptoms_to_set.append(symptom)
+            
+            obj.related_symptoms.set(symptoms_to_set)
+
+            if original_specialization:
+                old_group = DoctorSymptomGroup.objects.filter(name=original_specialization).first()
+                if old_group:
+                    old_group.doctors.remove(obj)
+            
+            new_group, created_new_group = DoctorSymptomGroup.objects.get_or_create(name=new_specialization)
+            new_group.doctors.add(obj)
+            if symptoms_to_set:
+                new_group.symptoms.add(*symptoms_to_set)
+            
 
 admin.site.register(Doctor, DoctorAdmin)
-admin.site.register(Symptom)
-admin.site.register(Appointment)
-admin.site.register(Contact)
-admin.site.register(CustomUser)
+admin.site.register(Symptom) 
+admin.site.register(Appointment) 
+admin.site.register(Contact) 
+admin.site.register(CustomUser) 
